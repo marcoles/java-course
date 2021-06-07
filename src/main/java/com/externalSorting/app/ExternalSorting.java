afd51fac00ded886f6d9427afa8386aaced58461
marcoles <oles.marcn@gmail.com>
2021-06-07 14:49:57 +0200

    fourth commit

--- src/main/java/com/externalSorting/app/ExternalSorting.java
+++ src/main/java/com/externalSorting/app/ExternalSorting.java
@@ -1,24 +1,42 @@
 package com.externalSorting.app;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 
 public class ExternalSorting {
-    protected static final int CHANGE_FILE = 500000;
-    private static int fileNumber = 0;
+    /*
+    CHANGE_FILE
+    Maximum number of lines of text that can be put into a single temporary file
+    */
+    static final int CHANGE_FILE = 400000;
 
-    public static List<String> readAndSplitData(String fileName) {
+    /*
+    Defines a default string comparator
+    */
+    public static Comparator<String> comparator = (o1, o2) -> o1.compareTo(o2);
+
+    /*
+    Reads data from a single text file and splits it into a number of temporary text files
+    containing lists of alphabetically sorted words
+
+    *param* fileName
+    Path to original single text file to be read from
+
+    *param* directory
+    Path of the directory which will contain temporary text files with lists of sorted words
+
+    *return*
+    Returns a list of created temporary files containing sorted data from original file
+    */
+    private static List<File> readAndSplitData(String filePath, File directoryPath) {
         List<String> buffer = new ArrayList<>();
-        int count = 0;
+        List<File> files = new ArrayList<>();
 
-        try (BufferedReader reader = Helper.openReader(fileName)) {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            int count = 0;
             for (String line = reader.readLine(); line != null; line = reader.readLine()){
                 List<String> splitLine = Arrays.asList(formatString(line));
                 count++;
@@ -27,37 +45,147 @@ public class ExternalSorting {
                 // buffer.remove("");  // remove empty strings in case of larger space between lines of text
                 if (count >= CHANGE_FILE) {
                     count = 0;
-                    sortAndWriteSplitData(buffer);
+                    files.add(sortAndWriteSplitData(buffer, directoryPath));
                     buffer.clear();
                 }
             }
+            files.add(sortAndWriteSplitData(buffer, directoryPath)); // write remaining data
+            buffer.clear();
+
         } catch(IOException e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
-        writeData(buffer, "src/main/resources/SmallLibraryOutput" + fileNumber + ".txt"); // write remaining data
-        return buffer;
+        return files;
     }
 
-    private static void sortAndWriteSplitData(List<String> buffer) {
-        sortAlphabetically(buffer);
-        writeData(buffer, "src/main/resources/SmallLibraryOutput" + fileNumber + ".txt");
-        fileNumber++;
-    }
+    /*
+    Sorts a list of strings using a default comparator
+    Then writes the list to a new temporary text file created in the given directory
 
-    public static void writeData(List<String> stringBuffer, String fileName) {
-        try (BufferedWriter writer = Helper.openWriter(fileName)) {
-            for (String s : stringBuffer) {
+    *param* buffer
+    List of strings to be sorted and written to the temporary file
+
+    *param* directory
+    Path of the directory which will contain temporary text files with lists of sorted words
+
+    *return*
+    Returns the created temporary file
+    */
+    private static File sortAndWriteSplitData(List<String> buffer, File directory) throws IOException {
+        buffer.sort(comparator);
+        File newFile = File.createTempFile("splitData", "flatFile", directory);
+        OutputStream out = new FileOutputStream(newFile);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
+            for (String s : buffer) {
                 writer.write(s);
                 writer.newLine();
             }
         } catch (IOException e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
+        return newFile;
     }
 
-    public static void sortAlphabetically(List<String> stringBuffer) {
-        Collections.sort(stringBuffer);
+    /*
+    Creates InputBuffer instances for every temporary text file and opens their BufferedReaders
+
+    *param* files
+    List of files that will be used to create InputBuffer instances
+
+    *return*
+    Returns a list of created InputBuffer instances
+    */
+    private static ArrayList<InputBuffer> convertFilesToInputBuffers(List<File> files) {
+        ArrayList<InputBuffer> buffers = new ArrayList<>();
+        for (File file : files) {
+            final int BUFFER_SIZE = 2000;
+            try {
+                BufferedReader reader = new BufferedReader(
+                                                new InputStreamReader(
+                                                        new FileInputStream(file)), BUFFER_SIZE);
+                InputBuffer buffer = new InputBuffer(reader);
+                buffers.add(buffer);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+            }
+        }
+        return buffers;
     }
+
+    /*
+    Takes a list of text files and merges them into a single text file while keeping an alphabetical order of words
+
+    *param* files
+    List of temporary text files containing data from the original file
+
+    *param* outputFile
+    Path to the file that will contain all the data from the original text file, sorted into a list of sorted words
+
+    *param* cmp
+    Comparator used as a base of the comparator in priority queue
+    */
+    private static void mergeFiles(List<File> files, File outputFile, Comparator<String> cmp) {
+        // creating a list of InputBuffer instances from files
+        ArrayList<InputBuffer> buffers = convertFilesToInputBuffers(files);
+
+        // creating a priority queue which will be used to store words into the output file
+        PriorityQueue<InputBuffer> priorityQueue = new PriorityQueue<>(10,
+                                                            (o1, o2) -> cmp.compare(o1.getCurrentString(), o2.getCurrentString()));
+
+        // try with resources for BufferedWriter
+        try (BufferedWriter writer = new BufferedWriter(
+                                            new OutputStreamWriter(
+                                                    new FileOutputStream(outputFile)))) {
+
+            // putting all the InputBuffer instances into the priority queue
+            for (InputBuffer buffer : buffers) {
+                if (!buffer.isEmpty()) {
+                    priorityQueue.add(buffer);
+                }
+            }
+            // writing words from priority queue into the output file until the queue is empty
+            while(priorityQueue.size() > 0) {
+                InputBuffer buffer = priorityQueue.poll();
+                String string = buffer.pop();
+                writer.write(string);
+                writer.newLine();
+                if (buffer.isEmpty()) {
+                    buffer.close();
+                } else {
+                    priorityQueue.add(buffer);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+
+        // closing readers from InputBuffer instances
+        try {
+            for (InputBuffer buffer : buffers) {
+                buffer.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+
+
+    }
+
+    /*
+    Takes a line of text and formats it by:
+        - changing all characters to lower case
+        - removing punctuation characters
+        - splitting it into an array of words
+
+     *param* inputString
+     Line of text to be formatted
+     */
     public static String[] formatString(String inputString) {
         return inputString
                 .toLowerCase()
@@ -65,24 +193,15 @@ public class ExternalSorting {
                 .split("\\s+");
     }
 
-    public static void getFileSize(String fileName) {
-        File file = new File(fileName);
-        if (!file.exists() || !file.isFile()) {
-            return;
-        }
-
-        System.out.println(file.length());
-    }
-
-    public static void getListSize(List<?> list) {
-        System.out.println(list.size());
-    }
 
     public static void main(String[] args){
-        // getFileSize("src/main/resources/SmallLibrary.txt");
-        /* List<String> text = */ readAndSplitData("src/main/resources/SmallLibrary.txt");
-        // sortAlphabetically(text);
-        //getListSize(text);
-        // writeData(text,"src/main/resources/SmallLibraryOutput.txt");
+        File newDirectory = new File("src/main/resources/tmpdir/");
+        List<File> files = readAndSplitData("src/main/resources/SmallLibrary.txt", newDirectory);
+        mergeFiles(files, new File("src/main/resources/SmallLibraryOutput.txt"), comparator);
+        for (File file : files) {
+            file.delete();
+        }
     }
+
+
 }
--- src/main/java/com/externalSorting/app/InputBuffer.java
+++ src/main/java/com/externalSorting/app/InputBuffer.java
@@ -1,2 +1,38 @@
-package com.externalSorting.app;public class InputBuffer {
+package com.externalSorting.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+public class InputBuffer {
+
+    private BufferedReader reader;
+    private String currentString;
+
+    public InputBuffer(BufferedReader r) throws IOException {
+        this.reader = r;
+        reload();
+    }
+
+    private void reload() throws IOException {
+        this.currentString = this.reader.readLine();
+    }
+
+    public void close() throws IOException {
+        this.reader.close();
+    }
+
+    public boolean isEmpty() {
+        return this.currentString == null;
+    }
+
+    public String getCurrentString() {
+        return this.currentString;
+    }
+
+    public String pop() throws IOException {
+        String string = getCurrentString().toString();
+        reload();
+        return string;
+    }
 }
