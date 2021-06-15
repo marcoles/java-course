6780bf1fc0dba4afa3ee2e65309a0ec136897e05
marcoles <oles.marcn@gmail.com>
2021-06-15 15:17:15 +0200

    trying to push only test folder

---
+++ src/test/java/com/externalSorting/testing/app/ExternalSortingTests.java
@@ -0,0 +1,41 @@
+package com.externalSorting.testing.app;
+
+import com.externalSorting.app.ExternalSortingRunner;
+import com.externalSorting.app.PathsData;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+
+
+public class ExternalSortingTests {
+
+    @Test
+    public void generalTest () {
+        String testFile = "src/main/resources/Library.txt";
+        String outputFolder = "src/main/resources";
+        PathsData paths = new PathsData(testFile, outputFolder);
+        ExternalSortingRunner runner = new ExternalSortingRunner(paths);
+
+        runner.externalSort();
+
+        try (BufferedReader reader = new BufferedReader(
+                                            new InputStreamReader(
+                                                    new FileInputStream(String.format("%s/Output.txt", outputFolder)))))
+        {
+            boolean flag = false;
+            String tmpLine = "";
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if (line.compareTo(tmpLine) < 0) {
+                    flag = true;
+                }
+                tmpLine = line;
+            }
+            Assert.assertFalse("Somewhere in the output text file words are not sorted " +
+                                            "according to the alphabetical order", flag);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
