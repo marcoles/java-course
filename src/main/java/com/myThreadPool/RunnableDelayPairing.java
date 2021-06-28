20d01398edb9b7b5cf0a10ccdfb61aeac9963a71
marcoles <oles.marcn@gmail.com>
2021-06-28 14:19:06 +0200

    changed pom.xml lombok dependency; used lombok annotation to set up a constructor for RunnableDelayPairing

--- src/main/java/com/myThreadPool/RunnableDelayPairing.java
+++ src/main/java/com/myThreadPool/RunnableDelayPairing.java
@@ -1,19 +1,16 @@
 package com.myThreadPool;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * This is a wrapper class to contain both the Runnable and its delay, after which it is supposed to execute
  */
 @Getter
+@RequiredArgsConstructor
 public class RunnableDelayPairing {
     private final Runnable task;
     private final long delay;
-
-    public RunnableDelayPairing(Runnable runnable, long delay) {
-        this.task = runnable;
-        this.delay = delay;
-    }
 
     public RunnableDelayPairing(Runnable runnable) {
         this.task = runnable;
